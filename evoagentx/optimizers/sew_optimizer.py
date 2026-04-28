fb6129c2b761d816a4aafeddbf8d44dd705ff8f0
jinyuan <fangjy6@gmail.com>
2026-04-28 22:15:28 +0100

    rename Optimizer class to WorkFlowOptimizer and remove workflow controller

--- evoagentx/optimizers/sew_optimizer.py
+++ evoagentx/optimizers/sew_optimizer.py
@@ -8,13 +8,14 @@ from copy import deepcopy
 import xml.etree.ElementTree as ET
 from typing import Literal, Union, Optional, List
 
-from .optimizer import Optimizer
 from ..core.logging import logger
+from ..core.module import BaseModule
 from ..models.base_model import BaseLLM 
 from ..benchmark.benchmark import Benchmark
+from ..evaluators.evaluator import Evaluator
 from ..workflow.action_graph import ActionGraph
 from ..core.callbacks import suppress_logger_info
-from ..workflow.workflow_graph import SequentialWorkFlowGraph
+from ..workflow.workflow_graph import WorkFlowGraph, SequentialWorkFlowGraph
 from ..prompts.workflow.sew_optimizer import mutation_prompts, thinking_styles
 
 VALID_SCHEMES = ["python", "yaml", "code", "core", "bpmn"]
@@ -653,7 +654,43 @@ class SimplePromptBreeder:
         return new_prompt
 
 
-class SEWOptimizer(Optimizer):
+class WorkFlowOptimizer(BaseModule):
+    
+    graph: Union[WorkFlowGraph, ActionGraph] = Field(description="The workflow to optimize.")
+    evaluator: Evaluator = Field(description="The evaluator to use for optimization.")
+
+    llm: BaseLLM = Field(default=None, description="The LLM to use for optimization and evaluation.")
+    max_steps: int = Field(default=5, description="The maximum number of optimization steps to take.")
+    eval_every_n_steps: int = Field(default=1, description="Evaluate the workflow every `eval_every_n_steps` steps.")
+    eval_rounds: int = Field(default=1, description="Run evaluation for `eval_rounds` times and compute the average score.")
+    convergence_threshold: int = Field(default=5, description="If the optimization has not improved the score for `convergence_threshold` steps, the optimization will be stopped.")
+
+    def optimize(self, dataset: Benchmark, **kwargs):
+        """
+        Optimize the workflow.
+        """
+        raise NotImplementedError(f"``optimize`` function for {type(self).__name__} is not implemented!")
+
+    def step(self, **kwargs):
+        """
+        Take a step of optimization.
+        """
+        raise NotImplementedError(f"``step`` function for {type(self).__name__} is not implemented!")
+    
+    def evaluate(self, dataset: Benchmark, eval_mode: str = "test", graph: Optional[Union[WorkFlowGraph, ActionGraph]] = None, **kwargs) -> dict:
+        """
+        Evaluate the workflow. If `graph` is provided, use the provided graph for evaluation. Otherwise, use the graph in the optimizer.
+        """
+        raise NotImplementedError(f"``evaluate`` function for {type(self).__name__} is not implemented!")
+    
+    def convergence_check(self, *args, **kwargs) -> bool:
+        """
+        Check if the optimization has converged.
+        """
+        raise NotImplementedError(f"``convergence_check`` function for {type(self).__name__} is not implemented!")
+    
+
+class SEWOptimizer(WorkFlowOptimizer):
 
     graph: Union[SequentialWorkFlowGraph, ActionGraph] = Field(description="The workflow to optimize.")
     repr_scheme: str = Field(default="python", description="The scheme to represent the workflow.")
